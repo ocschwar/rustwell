0b4731d02bdd49de73aa2d6576852fe07064ba2b
OMri <ocschwar@mit.edu>
2018-02-11 22:49:24 -0500

    Defining object stores for the database.
    Starting with local storage and CIFS.

---
+++ src/stores.rs
@@ -0,0 +1,15 @@
+pub enum ObjectStore {};
+// TO change to a type class at a later time.
+
+pub struct LocalStore {
+    mount_path: String,
+    
+};
+
+pub struct CifsShare {
+
+};
+
+pub struct removable {
+
+}
