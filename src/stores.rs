668ae022594cac771c2d656e255e2a1659ab7dc0
OMri <ocschwar@mit.edu>
2018-02-12 20:57:18 -0500

    Using semantics from AFS because I can.

--- src/stores.rs
+++ src/stores.rs
@@ -1,13 +1,23 @@
 pub trait CRUD {
     // All object stores in this framework should implement these.
-
+    //
+    // Using RLIDWKA as a shout out to the AFS community.
+    // https://www.cs.cmu.edu/~help/afs/afs_acls.html
     // The Option<> is to distinbuish between a PUT and a POST.
     // for filesystem stores, use mktemp??
+    // i
     fn create(&self,path:Option<String>)->Result<String> ;
+    // r
     fn read(&self,path:String)->Result<String> ;
-
-    // THis will need some refinement. 
+    // Obtain object meta data, listing a
+    // directory or a single resource
+    // l
+    fn list(&self,path:String)->Result<Vec<String>>> ;
+    //
+    fn write(&self,path:String, content:String) -> Result<String>;
+    // a - update resource meta data
     fn update(&self,path:String)->Result<String> ;
+    // d 
     fn delete(&self,path:String)->Result<String> ;
 
 }
