fd9d889b8cb6c1c5c8f272d9e45b1ade975f5b05
OMri <ocschwar@mit.edu>
2018-09-30 23:23:09 -0400

    Almost posting into Diesl.

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -5,13 +5,10 @@ extern crate rocket;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde;
-extern crate docopt;
+//extern crate docopt;
 //extern crate time;
 extern crate diesel;
 extern crate rustwell;
-extern crate r2d2_diesel;
-extern crate r2d2;
-extern crate dotenv;
 extern crate rocket_contrib;
 extern crate rexif;
 extern crate sha2;
@@ -25,14 +22,12 @@ use multipart::mock::StdoutTee;
 use multipart::server::Multipart;
 use multipart::server::save::Entries;
 use multipart::server::save::SaveResult::*;
-
 use rocket::Data;
 use rocket::http::{ContentType,Status};
 use rocket::response::Stream;
 use rocket::response::status::Custom;
 
-use self::dotenv::dotenv;
-use docopt::Docopt;
+//use docopt::Docopt;
 use std::str;
 use std::env;
 use std::fs::File;
@@ -41,32 +36,30 @@ use std::io::{self, Cursor, Write};
 use rustwell::*;
 use self::models::*;
 use diesel::prelude::*;
-//use rexif;
-use diesel::sqlite::SqliteConnection;
-use r2d2_diesel::ConnectionManager;
+
+//use diesel::sqlite::SqliteConnection;
 //use rocket::response::content::Json;
 use rocket_contrib::Json;
 use sha2::{Sha512,Digest};
-//use crypto::digest::Digest;
-//use rocket
-// An alias to the type for a pool of Diesel SQLite connections.
-type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
-
-
-
-/// Initializes a database pool.
-fn init_pool() -> Pool {
-    dotenv().ok();
-    // The URL to the database, set via the `DATABASE_URL` environment variable.
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
-    let manager = ConnectionManager::<SqliteConnection>::new(database_url);
-    r2d2::Pool::new(manager).expect("db pool")
-}
 
 use std::ops::Deref;
 use rocket::request::{self, FromRequest};
 use rocket::{Request, State, Outcome};
+
+//extern crate rocket;
+//extern crate diesel;
+extern crate dotenv;
+extern crate r2d2_diesel;
+extern crate r2d2;
+
+use self::dotenv::dotenv;
+use r2d2_diesel::ConnectionManager;
+
+//use diesel::prelude::*;
+use diesel::sqlite::SqliteConnection;
+
+//use rocket::request::{self, FromRequest};
+//use rocket::{Request, State, Outcome};
 
 // Connection request guard type: a wrapper around an r2d2 pooled connection.
 pub struct DbConn(pub r2d2::PooledConnection<ConnectionManager<SqliteConnection>>);
@@ -93,6 +86,21 @@ impl Deref for DbConn {
     fn deref(&self) -> &Self::Target {
         &self.0
     }
+}
+
+// An alias to the type for a pool of Diesel SQLite connections.
+type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+
+
+
+/// Initializes a database pool.
+fn init_pool() -> Pool {
+    dotenv().ok();
+    // The URL to the database, set via the `DATABASE_URL` environment variable.
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+    let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+    r2d2::Pool::new(manager).expect("db pool")
 }
 
 #[derive(FromForm)]
@@ -126,62 +134,62 @@ fn list_photos(conn: DbConn) -> Json<Vec<Photo>> {
 fn list_some_photos(conn: DbConn, query:Option<PhotoQuery>) -> Json<Vec<Photo>> {
     use self::schema::PhotoTable::dsl::*;
     let mut bq = PhotoTable.into_boxed();
-    match(query){
+    match query {
         None => (),
         Some(q) => {
-            bq = match(q.title){
+            bq = match q.title{
                 None=>bq,
                 Some(x)=>bq.filter(title.eq(x))
             };
-            bq = match(q.re_title){
+            bq = match q.re_title{
                 None=>bq,
                 Some(x)=>bq.filter(title.like(x))
             };
-            bq = match(q.developer){
+            bq = match q.developer{
                 None=>bq,
                 Some(x)=>bq.filter(developer.eq(x))
             };
-            bq = match(q.filename){
+            bq = match q.filename{
                 None=>bq,
                 Some(x)=>bq.filter(filename.eq(x))
             };
-            bq = match(q.md5){
+            bq = match q.md5{
                 None=>bq,
                 Some(x)=>bq.filter(md5.eq(x))
             };
-            bq = match(q.comment){
+            bq = match q.comment{
                 None=>bq,
                 Some(x)=>bq.filter(comment.eq(x))
             };
-            bq = match(q.created_after){
+            bq = match q.created_after{
                 None=>bq,
                 Some(x)=>bq.filter(time_created.gt(x))
             };
-            bq = match(q.limit){
+            bq = match q.limit{
                 None=>bq,
                 Some(x)=>bq.limit(x)
             };
-            bq = match(q.offset){
+            bq = match q.offset{
                 None=>bq,
                 Some(x)=>bq.offset(x)
             };
-            bq = match(q.id){
+            bq = match q.id{
                 None=>bq,
                 Some(x)=>bq.filter(id.eq(x))
             };
-            bq = match(q.created_before){
+            bq = match q.created_before{
                 None=>bq,
                 Some(x)=>bq.filter(time_created.lt(x))
             };
-            bq = match(q.re_comment){
+            bq = match q.re_comment{
                 None=>bq,
                 Some(x)=>bq.filter(comment.like(x))
             };
-            bq = match(q.transformations){
+            bq = match q.transformations{
                 None=>bq,
                 Some(x)=>bq.filter(transformations.eq(x))
             };
-            bq = match(q.backlinks){
+            bq = match q.backlinks{
                 None=>bq,
                 Some(x)=>bq.filter(backlinks.eq(x))
             };
@@ -238,7 +246,10 @@ fn get_photo(conn:DbConn, ID:i32) -> Vec<u8> {
 
 #[post("/upload", data = "<data>")]//, format = "multipart/form-data")]
 // signature requires the request to have a `Content-Type`
-fn multipart_upload(data: Data,cont_type:&ContentType) -> std::result::Result<Stream<Cursor<Vec<u8>>>, Custom<String>> {
+fn multipart_upload(
+    conn:DbConn,
+    data: Data,
+    cont_type:&ContentType) -> std::result::Result<Stream<Cursor<Vec<u8>>>, Custom<String>> {
     if !cont_type.is_form_data() {
         return Err(Custom(
             Status::BadRequest,
@@ -252,27 +263,28 @@ fn multipart_upload(data: Data,cont_type:&ContentType) -> std::result::Result<St
             )
         )?;
 
-    match process_upload(boundary, data) {
+    match process_upload(boundary, data ,conn) {
         Ok(resp) => Ok(Stream::from(Cursor::new(resp))),
         Err(err) => Err(Custom(Status::InternalServerError, err.to_string()))
     }
 }
 
-fn process_upload(boundary: &str, data: Data) -> io::Result<Vec<u8>> {
+fn process_upload(boundary: &str, data: Data,    conn:DbConn
+) -> io::Result<Vec<u8>> {
     let mut out = Vec::new();
 
     // saves all fields, any field longer than 10kB goes to a temporary directory
     // Entries could implement FromData though that would give zero control over
     // how the files are saved; Multipart would be a good impl candidate though
     match Multipart::with_body(data.open(), boundary).save().temp() {
-        Full(entries) => process_entries(entries, &mut out)?,
+        Full(entries) => process_entries(entries, &mut out,conn)?,
         Partial(partial, reason) => {
             writeln!(out, "Request partially processed: {:?}", reason)?;
             if let Some(field) = partial.partial {
                 writeln!(out, "Stopped on field: {:?}", field.source.headers)?;
             }
 
-            process_entries(partial.entries, &mut out)?
+            process_entries(partial.entries, &mut out,conn)?
         },
         multipart::server::save::SaveResult::Error(e) => return Err(e),
     }
@@ -282,7 +294,7 @@ fn process_upload(boundary: &str, data: Data) -> io::Result<Vec<u8>> {
 
 // having a streaming output would be nice; there's one for returning a `Read` impl
 // but not one that you can `write()` to
-fn process_entries(entries: Entries, mut out: &mut Vec<u8>) -> io::Result<()> {
+fn process_entries(entries: Entries, mut out: &mut Vec<u8>,conn:DbConn) -> io::Result<()> {
     {
         let stdout = io::stdout();
         let tee = StdoutTee::new(&mut out, &stdout);
@@ -290,10 +302,16 @@ fn process_entries(entries: Entries, mut out: &mut Vec<u8>) -> io::Result<()> {
             println!("{} {:?}",k,v);
             for f in v{
                 println!("{:?}",f);                                
-                match &f.headers.filename {
-                    Some(x) => println!("FName {}",x),
-                    None => println!("NoName")
-                }
+                let fname=match &f.headers.filename {
+                    Some(x) => { //let fname = x;
+                        println!("FName {}",x);
+                        x
+                    },
+                    None => {
+                        println!("NoName");
+                        ""
+                    }
+                };
                 println!("{:?}",f.data.size());
                 let mut r = f.data.readable();
                 match r {
@@ -301,7 +319,11 @@ fn process_entries(entries: Entries, mut out: &mut Vec<u8>) -> io::Result<()> {
                         println!("readable ");
                         let mut ibuffer = Vec::new();
                         // read the whole file
+                                     
                         R.read_to_end(&mut ibuffer).expect("File unread");
+                        create_photo(&conn,
+                                     fname,
+                                     &ibuffer);
                         let mut hasher = Sha512::new();
 
                         // write input message
@@ -346,6 +368,29 @@ struct Args {
     flag_addr: String
 }
 
+fn create_photo(conn: &DbConn,
+                filename: &str,
+                buf : &Vec<u8>
+) -> usize {
+    use self::schema::PhotoTable::dsl::*;    
+    let mut new_photo = Photo {
+        ..Default::default()
+    };
+    match rexif::parse_buffer(&buf) {
+        Ok(exif) => {
+            println!("EXIF {:?}",&exif);
+        },
+        Err(e) =>{
+            println!("noexif {:?}",&e);
+        }
+    }
+    
+    diesel::insert_into(PhotoTable)
+        .values(&new_photo)
+        .execute(conn)//get_result(&*conn)
+        .expect("Error saving new Photo")
+}
+
 
 fn main() {
     rocket::ignite()
@@ -354,5 +399,4 @@ fn main() {
         .manage(init_pool())
         .launch();
     // TODO: read each resource, dump some contents..
-    // dotenv the port #
 }
